89866b3f001aeb8fc179ec4082d7109e956b0d85
Marcos Lozada <marcoslozadaf@gmail.com>
2021-10-05 11:13:55 -0300

    Add email

--- pages/index.js
+++ pages/index.js
@@ -41,7 +41,7 @@ export default function Home() {
                 <div className='py-2 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
                   <dt className='text-sm font-medium text-gray-400'>E-mail</dt>
                   <dd className='mt-1 text-sm text-gray-900 dark:text-white sm:mt-0 sm:col-span-2'>
-                    marcoslozadaf@gmail.com | marcos@baas256.com
+                  marcos.lozada@ucc.edu.ar | marcos@baas256.com | marcoslozadaf@gmail.com
                   </dd>
                 </div>
                 <div className='py-2 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
